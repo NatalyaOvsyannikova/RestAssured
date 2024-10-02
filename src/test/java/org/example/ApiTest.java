53e0aa0a8331f8ee1096c6f3869b5d6bd518d030
Овсянникова Наталья (Партнер) <natalya_ovsyannikova@rntgroup.com>
2024-10-02 13:46:08 +0300

    Задание на модуль 9 - правки

--- src/test/java/org/example/ApiTest.java
+++ src/test/java/org/example/ApiTest.java
@@ -1,71 +1,113 @@
 package org.example;
 
-import io.restassured.RestAssured;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.config.RestAssuredConfig;
 import io.restassured.config.SSLConfig;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.config;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
 
 /**
  * API tests
  */
 public class ApiTest
-
 {
-   @BeforeClass
-    public void setBaseUri() {
-       RestAssured.baseURI = "https://reqres.in/api";
-   }
+    public static RequestSpecification requestSpec = new RequestSpecBuilder().setBaseUri("https://reqres.in")
+            .setBasePath("/api").build().config(new RestAssuredConfig()
+                    .sslConfig(new SSLConfig().relaxedHTTPSValidation()));
+    class User {
+        public String name;
+        public String job;
+
+        public User(String name, String job) {
+            this.name = name;
+            this.job = job;
+        }
+    }
+    @JsonIgnoreProperties (ignoreUnknown = true)
+    public class UserResponsePOJO {
+
+        public Data data;
+        public Support support;
+    }
+    public class Data {
+
+        public Long id;
+        public String email;
+        public String first_name;
+        public String last_name;
+        public String avatar;
+    }
+
+    public class Support {
+        public String url;
+        public String text;
+    }
 
    @Test(description = "Test Get Users")
     public void testGetUsers() {
-      RestAssured.config = config().sslConfig(new SSLConfig().relaxedHTTPSValidation());
 
-      Response res = given()
-                    .log().all()
-                    .contentType(ContentType.JSON)
-               .when()
-                    .get("users?page=2")
-               .then()
-                    .log().all()
-                    .statusCode(200)
-                    .extract().response();
-      int total = res.jsonPath().getInt("total");
-      Assert.assertEquals(total, 12, "Total is not 12");
+      given()
+              .log().all()
+              .spec(requestSpec)
+              .contentType(ContentType.JSON)
+      .when()
+              .get("users?page=2")
+      .then()
+              .log().all()
+              .statusCode(200)
+              .body("total", equalTo (12));
 
    }
 
     @Test(description = "Test Single User")
     public void testSingleUser() {
-        RestAssured.config = config().sslConfig(new SSLConfig().relaxedHTTPSValidation());
 
         Response res = given()
                 .log().all()
+                .spec(requestSpec)
                 .contentType(ContentType.JSON)
         .when()
                 .get("users/2")
         .then()
                 .log().all()
                 .statusCode(200)
+                .body("data.first_name", equalTo("Janet"))
                 .extract().response();
-        String firstName = res.jsonPath().getString("data.first_name");
-        Assert.assertEquals(firstName, "Janet", "First name is not Janet");
+        /* JsonObject jo = new Gson().fromJson(res.asPrettyString(), JsonObject.class);
+        assertThat("First name is not Janet", jo.get("data").getAsJsonObject().get("first_name").getAsString(), is("Janet"));
+        assertThat("Last name is not Janet", jo.get("data").getAsJsonObject().get("last_name").getAsString(), is("Weaver"));
+        assertThat("Last name is not Janet", jo.get("data").getAsJsonObject().get("email").getAsString(), is("janet.weaver@reqres.in"));*/
+        UserResponsePOJO serializedResponse = res.as(UserResponsePOJO.class);
+        Data data = serializedResponse.data;
+        assertThat("First name is not Janet", data.first_name, is("Janet"));
+
 
     }
 
     @Test(description = "Test Single User Not Found")
     public void testSingleUserNotFound() {
-        RestAssured.config = config().sslConfig(new SSLConfig().relaxedHTTPSValidation());
 
         given()
                 .log().all()
+                .spec(requestSpec)
                 .contentType(ContentType.JSON)
         .when()
                 .get("users/23")
@@ -77,110 +119,99 @@ public class ApiTest
     }
     @Test(description = "Test Get Resources")
     public void testGetResources() {
-        RestAssured.config = config().sslConfig(new SSLConfig().relaxedHTTPSValidation());
 
         Response res = given()
                 .log().all()
+                .spec(requestSpec)
                 .contentType(ContentType.JSON)
         .when()
                 .get("unknown")
         .then()
                 .log().all()
                 .statusCode(200)
+                //.body("total", equalTo(12))
                 .extract().response();
-        int total = res.jsonPath().getInt("total");
-        Assert.assertEquals(total, 12, "Total is not 12");
+        Map<String, Object> map = res.as(Map.class);
+        assertThat("Total is 12", map.get("total").equals(12));
+        assertThat("There are 6 resources in response", ((List)map.get("data")).size(), is(6));
 
     }
 
     @Test(description = "Test Single Resource")
     public void testSingleResource() {
-        RestAssured.config = config().sslConfig(new SSLConfig().relaxedHTTPSValidation());
 
-        Response res = given()
+        given()
                 .log().all()
+                .spec(requestSpec)
                 .contentType(ContentType.JSON)
         .when()
                 .get("unknown/2")
         .then()
                 .log().all()
                 .statusCode(200)
-                .extract().response();
-        String name = res.jsonPath().getString("data.name");
-        Assert.assertEquals(name, "fuchsia rose", "Name is not fuchsia rose");
+                .body("data.name", equalTo("fuchsia rose"));
 
     }
 
     @Test(description = "Test Single Resource Not Found")
     public void testSingleResourceNotFound() {
-        RestAssured.config = config().sslConfig(new SSLConfig().relaxedHTTPSValidation());
 
-        Response res = given()
+        given()
                 .log().all()
+                .spec(requestSpec)
                 .contentType(ContentType.JSON)
         .when()
                 .get("unknown/23")
         .then()
                 .log().all()
                 .statusCode(404)
-                .extract().response();
-        Assert.assertEquals(res.body().asString(), "{}", "Body is not empty");
+                .body(is("{}"));
 
     }
 
     
    @Test(description = "Test Post User")
     public void testPostUser() {
-       RestAssured.config = config().sslConfig(new SSLConfig().relaxedHTTPSValidation());
 
-       String requestBody = "{" +
-               "\"name\": \"Ivan\"," +
-               "\"job\": \"programmer\"" +
-               "}";
+       User user = new User("Ivan", "programmer");
 
-       Response res = given()
-                            .log().all()
-                            .contentType(ContentType.JSON)
-                            .body(requestBody)
-                            .post("users")
-                .then()
-                            .log().all()
-                            .statusCode(201)
-                            .extract().response();
-       String id = res.jsonPath().getString("id");
-       Assert.assertFalse(id.isEmpty(), "Id should not be empty");
+       given()
+               .log().all()
+               .spec(requestSpec)
+               .contentType(ContentType.JSON)
+               .body(user)
+               .post("users")
+       .then()
+               .log().all()
+               .statusCode(201)
+               .body("id", notNullValue())                            ;
 
    }
 
     @Test(description = "Test Update User")
     public void testUpdateUser() {
-        RestAssured.config = config().sslConfig(new SSLConfig().relaxedHTTPSValidation());
 
-        String requestBody = "{" +
-                "\"name\": \"morpheus\"," +
-                "\"job\": \"zion resident\"" +
-                "}";
+        User user = new User("morpheus", "zion resident");
 
-        Response res = given()
+        given()
                 .log().all()
+                .spec(requestSpec)
                 .contentType(ContentType.JSON)
-                .body(requestBody)
+                .body(user)
                 .patch("users/2")
         .then()
                 .log().all()
                 .statusCode(200)
-                .extract().response();
-        String name = res.jsonPath().getString("name");
-        Assert.assertEquals(name, "morpheus", "Name is not morpheus");
+                .body("name", equalTo("morpheus"));
 
     }
 
     @Test(description = "Test Delete User")
     public void testDeleteUser() {
-        RestAssured.config = config().sslConfig(new SSLConfig().relaxedHTTPSValidation());
 
         given()
                 .log().all()
+                .spec(requestSpec)
                 .delete("users/2")
         .then()
                 .log().all()
@@ -190,37 +221,35 @@ public class ApiTest
 
     @Test(description = "Test Register User")
     public void testRegisterUser() {
-        RestAssured.config = config().sslConfig(new SSLConfig().relaxedHTTPSValidation());
 
-        String requestBody = "{" +
-                "\"email\": \"eve.holt@reqres.in\"," +
-                "\"password\": \"pistol\"" +
-                "}";
+        Map<String, String> requestBody = new HashMap<>() {{
+            put ("email", "eve.holt@reqres.in");
+            put ("password", "pistol");
+        }};
 
-        Response res = given()
+        given()
                 .log().all()
+                .spec(requestSpec)
                 .contentType(ContentType.JSON)
                 .body(requestBody)
                 .post("register")
         .then()
                 .log().all()
                 .statusCode(200)
-                .extract().response();
-        String id = res.jsonPath().getString("id");
-        Assert.assertFalse(id.isEmpty(), "Id should not be empty");
+                .body("id", notNullValue());
 
     }
 
     @Test(description = "Test Register User without password")
     public void testRegisterUserError() {
-        RestAssured.config = config().sslConfig(new SSLConfig().relaxedHTTPSValidation());
-
-        String requestBody = "{" +
-                "\"email\": \"eve.holt@reqres.in\"" +
-                "}";
+        String requestBody = """
+        {
+            "email": "eve.holt@reqres.in"            
+        }""";
 
         given()
                 .log().all()
+                .spec(requestSpec)
                 .contentType(ContentType.JSON)
                 .body(requestBody)
                 .post("register")
@@ -232,37 +261,35 @@ public class ApiTest
 
     @Test(description = "Test Login User")
     public void testLoginUser() {
-        RestAssured.config = config().sslConfig(new SSLConfig().relaxedHTTPSValidation());
 
-        String requestBody = "{" +
-                "\"email\": \"eve.holt@reqres.in\"," +
-                "\"password\": \"cityslicka\"" +
-                "}";
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("email", "eve.holt@reqres.in");
+        jsonObject.addProperty("password", "cityslicka");
 
-        Response res = given()
+        given()
                 .log().all()
+                .spec(requestSpec)
                 .contentType(ContentType.JSON)
-                .body(requestBody)
+                .body(jsonObject.toString())
                 .post("login")
         .then()
                 .log().all()
                 .statusCode(200)
-                .extract().response();
-        String token = res.jsonPath().getString("token");
-        Assert.assertFalse(token.isEmpty(), "Token should not be empty");
+                .body("token", notNullValue());
 
     }
 
     @Test(description = "Test Login User without password")
     public void testLoginUserError() {
-        RestAssured.config = config().sslConfig(new SSLConfig().relaxedHTTPSValidation());
 
-        String requestBody = "{" +
-                "\"email\": \"peter@klaven\"" +
-                "}";
+        String requestBody = """
+        {
+            "email": "peter@klaven"            
+        }""";
 
         given()
                 .log().all()
+                .spec(requestSpec)
                 .contentType(ContentType.JSON)
                 .body(requestBody)
                 .post("register")
@@ -273,10 +300,10 @@ public class ApiTest
     }
     @Test(description = "Test Delayed Response")
     public void testDelayedResponse() {
-        RestAssured.config = config().sslConfig(new SSLConfig().relaxedHTTPSValidation());
 
         Response res = given()
                 .log().all()
+                .spec(requestSpec)
                 .contentType(ContentType.JSON)
         .when()
                 .get("users?delay=2")
